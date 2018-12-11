7e1790703f3a40fa20db1973384752975d7a7c94
Vijay Kiran <vijaykiran225@gmail.com>
2018-12-12 00:24:40 +0530

    adding eventbus to react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import EventBus from 'vertx3-eventbus-client'
+
 
 class App extends Component {
+
+constructor(){
+	super();
+
+	this.state = {
+		
+		content : ''
+	};
+}
+
+
   render() {
+
+let getContents = (str) => {
+
+	return "some text "+ str;
+}
+  	let eb = new EventBus('http://localhost:8080/eventbus/');
+  	eb.onopen = () => {
+	  eb.registerHandler('feed',  (err, msg) => {
+	    console.log(msg.body.now);
+	    this.setState({content : msg.body.now});
+	  });	
+	};
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+            	{getContents(this.state.content)}
           </p>
           <a
             className="App-link"
@@ -23,6 +49,7 @@ class App extends Component {
       </div>
     );
   }
+
 }
 
 export default App;
